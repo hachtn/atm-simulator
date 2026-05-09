0f66a67ffc1de70cf0cc3caf46dbe35e84ff270b
hachtn <email_github_cua_ban@gmail.com>
2026-05-10 01:48:28 +0900

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/atm/atm_simulator/AtmSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.atm.atm_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atm/atm_simulator/AtmSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atm.atm_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
